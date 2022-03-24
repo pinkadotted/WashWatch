03664bf498c629221a74ccdee6f70f954d54a3e5
ShawnCsj <61451612+shyawn@users.noreply.github.com>
2022-03-24 22:26:03 +0800

    Updated MainActivity with 3 Block buttons
    Might need to change to the other kind of button

--- WashWashes/app/src/main/java/com/example/washwash1/MainActivity.java
+++ WashWashes/app/src/main/java/com/example/washwash1/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.washwash1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton androidImageButton;
+    ImageButton androidImageButton1;
+    ImageButton androidImageButton2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        androidImageButton = findViewById(R.id.block55);
+        androidImageButton1 = findViewById(R.id.block57);
+        androidImageButton2 = findViewById(R.id.block59);
+
+        androidImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, Washer.class));
+            }
+        });
+
+        androidImageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, Washer.class));
+            }
+        });
+
+        androidImageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, Washer.class));
+            }
+        });
     }
 }
