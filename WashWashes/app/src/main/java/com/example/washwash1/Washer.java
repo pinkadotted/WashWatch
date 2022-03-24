0c1364f0a7fe2078cd9b29610b35ae33a7c50cfe
ShawnCsj <61451612+shyawn@users.noreply.github.com>
2022-03-24 22:27:11 +0800

    Added Washer Activity for Washer page

---
+++ WashWashes/app/src/main/java/com/example/washwash1/Washer.java
@@ -0,0 +1,18 @@
+package com.example.washwash1;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Washer extends AppCompatActivity {
+    ArrayList<Integer> images;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.washer);
+
+    }
+}
