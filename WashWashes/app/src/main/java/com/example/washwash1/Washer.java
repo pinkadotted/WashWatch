16771a7b2b2da4fac27d5f1db1a3a2d1d6634e79
ShawnCsj <61451612+shyawn@users.noreply.github.com>
2022-03-25 11:55:08 +0800

    Edited Washer

--- WashWashes/app/src/main/java/com/example/washwash1/Washer.java
+++ WashWashes/app/src/main/java/com/example/washwash1/Washer.java
@@ -1,18 +1,40 @@
 package com.example.washwash1;
 
-import android.os.Bundle;
-import android.widget.ImageView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Washer extends AppCompatActivity {
-    ArrayList<Integer> images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.washer);
+        setContentView(R.layout.activity_washer);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.washer);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.dryer:
+                        startActivity(new Intent(getApplicationContext(), Dryer.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.washer:
+                        return true;
+                }
+                return false;
+            }
+        });
 
     }
 }
