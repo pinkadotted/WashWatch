b15e4dba6bed6e1017336f01e3e85d817f10570e
IvanFengJK <ivanfengjunkai1997114@gmail.com>
2022-03-28 06:08:16 +0800

    successfully import mongodb realm library

--- WashWashes/app/src/main/java/com/example/washwash1/MainActivity.java
+++ WashWashes/app/src/main/java/com/example/washwash1/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Realm.init(this);
         setContentView(R.layout.activity_main);
 
         Button block55 = findViewById(R.id.block55);
