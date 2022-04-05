46cca35ebf29804f60a87888241ed174f6584ca9
ShawnCsj <csjshawn@gmail.com>
2022-04-05 12:30:43 +0800

    Edited files for recyclerview

--- WashWashes/app/src/main/java/com/example/washwash1/Washer.java
+++ WashWashes/app/src/main/java/com/example/washwash1/Washer.java
@@ -2,14 +2,30 @@ package com.example.washwash1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Washer extends AppCompatActivity {
 
-    // to ensure that the app goes to MainActivity when the android back button is pressed
+    RecyclerView recyclerView;
+
+//    List<String> s1;
+//    List<String> s2;
+//    List<Integer> images;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.redmachine, R.drawable.redmachine, R.drawable.redmachine,
+            R.drawable.greenmachine, R.drawable.greenmachine, R.drawable.greenmachine,
+            R.drawable.redmachine, R.drawable.redmachine, R.drawable.redmachine,
+            R.drawable.greenmachine, R.drawable.greenmachine, R.drawable.greenmachine};
+
+    // to ensure that the app goes to Washer when the android back button is pressed
     @Override
     public void onBackPressed() {
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -21,8 +37,17 @@ public class Washer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_washer);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        recyclerView = findViewById(R.id.recyclerView);
 
+        s1 = getResources().getStringArray(R.array.washers);
+        s2 = getResources().getStringArray(R.array.washers_times);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.washer);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -32,10 +57,6 @@ public class Washer extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(), Dryer.class));
                         overridePendingTransition(0,0);
                         return true;
-//                    case R.id.home:
-//                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-//                        overridePendingTransition(0,0);
-//                        return true;
                     case R.id.washer:
                         return true;
                 }
